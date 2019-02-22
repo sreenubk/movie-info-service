762e25e31e0e797a7a13d2822ca3f9991d9eb0b4
srinivasa rao <srinivasarao@srinivasas-mini.attlocal.net>
2019-02-22 07:29:04 -0800

    my first version

---
+++ src/main/java/io/lotus/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,21 @@
+package io.lotus.movieinfoservice.resources;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.lotus.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+		
+		return (new Movie(movieId, "Transformer"));
+		
+	}
+
+}
